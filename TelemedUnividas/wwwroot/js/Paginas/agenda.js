9bee177b2de706c4551c587b64364638927839e0
Fe-lipe-Alves <felipealves1550@gmail.com>
2020-10-30 15:13:57 -0300

    Ajuste layuot agenda

--- TelemedUnividas/wwwroot/js/Paginas/agenda.js
+++ TelemedUnividas/wwwroot/js/Paginas/agenda.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-    
+    $('.dia-semana').on('click', function () {
+        $('#calendario .ativo').removeClass('ativo');
+        $(this).addClass('ativo');
+    });
 });
